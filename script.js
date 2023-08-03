a385c54ccc64a72c68182be63567d75644bf9f23
gizatupu <30436045+gizatupu@users.noreply.github.com>
2023-08-03 04:06:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -140,4 +140,17 @@ function handleCardClick(event) {
 createDivsForColors(shuffledColors);
 
 
+//NOTES:
+//1. You have a mix of camelCase and PascalCase naming conventions. It's good practice to stick to one convention throughout your code. For example, you have picksFlipped and pickFlipped.
+//2.Your code is mostly self-explanatory, but adding some comments to describe the purpose of certain functions, variables, or logic blocks can make it easier for others (or your future self) to understand the code at a glance.
+//3.You might want to consider adding some CSS styles to make the game visually appealing. You could style the cards, the game container, and provide some feedback when cards are clicked (e.g., changing their appearance temporarily to indicate a match or a mismatch).
+//4.The handleCardClick function is quite long and contains nested if-else blocks. While it works, it might be more readable and maintainable if you break down the logic into smaller, separate functions. This could help make your code more modular and easier to extend in the future.
+//5.There is a typo in your code. You have pickFlipped instead of picksFlipped in the else block after the cards don't match. This might cause an issue.
+//6.Consider using a consistent timeout delay for flipping unmatched cards back over. Right now, you have a delay of 1000ms (1 second), but it's used in different places with different conditions. It might be clearer to have a single delay value for this purpose.
+//7.It would be nice to provide a congratulatory message when the player successfully matches all the cards, instead of just the "GAME OVER" message. This would give a sense of accomplishment.
+//8.Consider adding a restart or play-again option after the game ends, so players don't need to refresh the entire page to play again.
+//9.Organize your code into sections or use a modular approach (e.g., separating the shuffling logic, game logic, and event listeners).
+
+
+
 
